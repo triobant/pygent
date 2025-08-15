a2bbac4ba91ead2a7b8fd6cd5f8191b4a91a0183
Brian Ott <bott@b1-systems.de>
2025-08-15 17:08:19 +0200

    update: more verbose output

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
 
+    verbose = "--verbose" in sys.argv
     args = sys.argv[1:]
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How od I build a calculator app?"')
         sys.exit(1)
 
@@ -20,17 +24,23 @@ def main():
     client = genai.Client(api_key=api_key)
 
     user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages)
+    generate_content(client, messages, verbose)
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     
     print("Response:")
     print(response.text)
