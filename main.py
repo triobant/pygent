2a0f70895889069350999001f150bd071da82570
Brian Ott <bott@b1-systems.de>
2025-08-15 16:58:12 +0200

    update: user prompt from args cli

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+import sys
 import os
-from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+from dotenv import load_dotenv
 
 
 def main():
     load_dotenv()
 
-    api_key = os.environ.get("GEMINI_API_KEY")
+    args = sys.argv[1:]
 
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How od I build a calculator app?"')
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    messages = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    user_prompt = " ".join(args)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     generate_content(client, messages)
 
 def generate_content(client, messages):
@@ -18,8 +32,6 @@ def generate_content(client, messages):
         contents=messages,
     )
     
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
 
