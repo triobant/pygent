e25e3362e29f83414300f0e98c390ccd5d100ecb
Brian Ott <bott@b1-systems.de>
2025-08-15 16:52:40 +0200

    update: first prompt to llm

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from pygent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+
+def main():
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+    messages = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    generate_content(client, messages)
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
