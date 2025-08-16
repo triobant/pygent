133160396dbe3d116ccf9805029d05651e0a47d5
Brian Ott <bott@b1-systems.de>
2025-08-16 22:52:20 +0200

    update: test cases to test only the new function

--- tests.py
+++ tests.py
@@ -1,24 +1,22 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 def test():
-    result = get_files_info("calculator", ".")
-    print("Result for current directory:")
-    print(result)
-    print("")
-
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
+    result = get_file_content("calculator", "main.py")
+    print("Result for 'main.py' content:")
     print(result)
 
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin' directory:")
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for 'pkg/calculator' content:")
     print(result)
 
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for '/bin/cat' content:")
     print(result)
 
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    print("Result for 'pkg/does_not_exist.py' content:")
+    print(result)
 
 if __name__ == "__main__":
     test()
