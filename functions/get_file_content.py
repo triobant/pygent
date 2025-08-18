bb325ef9a3da82881f3423c6d8fe1a1ac1201c85
Brian Ott <bott@b1-systems.de>
2025-08-18 19:40:58 +0200

    update: removed duplicate function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -18,24 +18,3 @@ def get_file_content(working_directory, file_path):
                 return file_content_string
     except Exception as e:
         return f'Error: could not open and read file "{file_path}": "{e}"'
-
-
-
-
-    def get_file_content(working_directory, file_path):
-    abs_working_dir = os.path.abspath(working_directory)
-    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
-    if not abs_file_path.startswith(abs_working_dir):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(abs_file_path):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    try:
-        with open(abs_file_path, "r") as f:
-            content = f.read(MAX_CHARS)
-            if os.path.getsize(abs_file_path) > MAX_CHARS:
-                content += (
-                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
-                )
-        return content
-    except Exception as e:
-        return f'Error reading file "{file_path}": {e}'
