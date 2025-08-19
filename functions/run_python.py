bf0b6365487e19b989f9b316009364b08f858c85
Brian Ott <bott@b1-systems.de>
2025-08-19 22:48:54 +0200

    update: check a few conditions

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,4 +1,11 @@
 import os
 
 def run_python_file(working_directory, file_path, args=[]):
-    ...
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if not abs_file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
