9b82f98d81f8ce30c1715c1e3fb3f94060cb60df
Brian Ott <bott@b1-systems.de>
2025-08-20 20:28:53 +0200

    update: import subprocess module trying to run a subprocess catching stdin stderr with a timeout of 30 seconds

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 def run_python_file(working_directory, file_path, args=[]):
     abs_working_dir = os.path.abspath(working_directory)
@@ -9,3 +10,7 @@ def run_python_file(working_directory, file_path, args=[]):
         return f'Error: File "{file_path}" not found.'
     if not abs_file_path.endswith(".py"):
         return f'Error: "{file_path}" is not a Python file.'
+    try:
+        subprocess.run(["python3", file_path] + args, cmd=working_directory, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=30)
+    except Exception as e:
+        return f'Error: could not run python file "{file_path}": "{e}"'
