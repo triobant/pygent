c2a611740c89cfd6867197387cd6c2cb711bc9ae
Brian Ott <bott@b1-systems.de>
2025-08-21 17:25:31 +0200

    update: built output string checkinf if returncode is 0 and stdout and stderr are not empty

--- functions/run_python.py
+++ functions/run_python.py
@@ -12,6 +12,11 @@ def run_python_file(working_directory, file_path, args=[]):
         return f'Error: "{file_path}" is not a Python file.'
     try:
         result = subprocess.run(["python3", file_path] + args, cwd=working_directory, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=30)
-        print(result)
+        output = f'STDOUT: {result.stdout}\nSTDERR: {result.stderr}'
+        if result.returncode != 0:
+            return f'{output}\nProcess exited with code {result.returncode}'
+        if not result.stdout and not result.stderr:
+            return f'No output produced.'
+        return output
     except Exception as e:
-        return f'Error: could not run python file "{file_path}": "{e}"'
+        return f"Error: executing Python file: {e}"
