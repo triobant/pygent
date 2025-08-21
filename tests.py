bd55edd0a65ceb396c27a182f1ab0cfddf9aa2f7
Brian Ott <bott@b1-systems.de>
2025-08-21 17:29:09 +0200

    update: changed tests for the current subject

--- tests.py
+++ tests.py
@@ -1,17 +1,25 @@
-from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print("Result for 'lorem.txt' content:")
+    result = run_python_file("calculator", "main.py")
+    print("Result for 'main.py' content:")
     print(result)
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print("Result for 'pkg/morelorem.txt' content:")
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
+    print("Result for 'main.py' content:")
     print(result)
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print("Result for '/tmp/temp.txt' content:")
+    result = run_python_file("calculator", "tests.py")
+    print("Result for 'tests.py' content:")
+    print(result)
+
+    result = run_python_file("calculator", "../main.py")
+    print("Result for '../main.py' content:")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print("Result for 'nonexistent.py' content:")
     print(result)
 
 if __name__ == "__main__":
