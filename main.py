710383a9c4d20b33efd15e0f97cf81243dc8ccb1
Brian Ott <bott@b1-systems.de>
2025-08-22 22:54:39 +0200

    update: available_functions as function param

--- main.py
+++ main.py
@@ -37,7 +37,9 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        )
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
