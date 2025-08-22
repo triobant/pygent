30d1d87cf08bfc218c7ceb63bf1b38b7ff5adac0
Brian Ott <bott@b1-systems.de>
2025-08-22 22:50:45 +0200

    update: schema_get_files_info and available_functions added

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,25 @@
 import os
+import types
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 def get_files_info(working_directory, directory="."):
     abs_working_dir = os.path.abspath(working_directory)
