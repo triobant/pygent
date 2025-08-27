dea074e2f4076e49aa533f89a3a8b3ee24f5feec
Brian Ott <bott@b1-systems.de>
2025-08-27 09:16:07 +0200

    update: fixed import

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,8 @@
 import os
 from google.genai import types
+from functions.get_file_content import schema_get_file_content
+from functions.run_python import schema_run_python_file
+from functions.write_file import schema_write_file
 
 schema_get_files_info = types.FunctionDeclaration(
     name="get_files_info",
