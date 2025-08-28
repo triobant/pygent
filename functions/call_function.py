fb3971a2716d064975025b2ccededf15aebec9a6
Brian Ott <bott@b1-systems.de>
2025-08-28 16:12:35 +0200

    update: check if function is valid name

--- functions/call_function.py
+++ functions/call_function.py
@@ -10,12 +10,35 @@ def call_function(function_call_part, verbose=False):
         print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     else:
         print(f" - Calling function: {function_call_part.name}")
-    
+
     operations = {
         'get_files_info': get_files_info,
         'get_file_content': get_file_content,
         'run_python_file': run_python_file,
         'write_file': write_file,
     }
-    function_call_part.args["working_directory"] = "./calculator"
-    result = operations[function_call_part.name](**function_call_part.args)
+
+    if function_call_part.name not in operations:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                name=function_call_part.name,
+                    response={"error": f"Unknown function: {function_call_part.name}"},
+                )
+            ],
+        )
+    else:
+        function_call_part.args["working_directory"] = "./calculator"
+        function_result = operations[function_call_part.name](**function_call_part.args)
+
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"result": function_result},
+                )
+            ],
+        )
+    
