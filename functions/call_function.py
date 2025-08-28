e59c537157e760f29e0525d901e223b557b5a6b1
Brian Ott <bott@b1-systems.de>
2025-08-28 16:05:13 +0200

    update: operations dictionary for callable functions

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,8 +1,21 @@
 import os
 from google.genai import types
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.run_python import run_python_file
+from functions.write_file import write_file
 
 def call_function(function_call_part, verbose=False):
     if verbose:
         print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     else:
         print(f" - Calling function: {function_call_part.name}")
+    
+    operations = {
+        'get_files_info': get_files_info,
+        'get_file_content': get_file_content,
+        'run_python_file': run_python_file,
+        'write_file': write_file,
+    }
+    function_call_part.args["working_directory"] = "./calculator"
+    result = operations[function_call_part.name](**function_call_part.args)
