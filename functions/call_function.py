b591f699862396d33a17b8500596eeb32ced3154
Brian Ott <bott@b1-systems.de>
2025-08-28 15:45:32 +0200

    update: check if verbose is needed

--- functions/call_function.py
+++ functions/call_function.py
@@ -2,3 +2,7 @@ import os
 from google.genai import types
 
 def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
